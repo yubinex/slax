dc613bb648d3c24f77d76fb9b29d492a934c9628
yubinex <janhauck99@gmail.com>
2025-09-25 10:18:58 +0200

    add event handlers to ChatRoomLive.Edit

--- lib/slax_web/live/chat_room_live/edit.ex
+++ lib/slax_web/live/chat_room_live/edit.ex
@@ -18,7 +18,7 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
         </:actions>
       </.header>
 
-      <.simple_form for={@form} id="room-form">
+      <.simple_form for={@form} id="room-form" phx-change="validate-room" phx-submit="save-room">
         <.input field={@form[:name]} type="text" label="Name" />
         <.input field={@form[:topic]} type="text" label="Topic" />
         <:actions>
@@ -40,6 +40,28 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
       |> assign_form(changeset)
 
     {:ok, socket}
+  end
+
+  def handle_event("save-room", %{"room" => room_params}, socket) do
+    case Chat.update_room(socket.assigns.room, room_params) do
+      {:ok, room} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Room updated successfully")
+         |> push_navigate(to: ~p"/rooms/#{room}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  def handle_event("validate-room", %{"room" => room_params}, socket) do
+    changeset =
+      socket.assigns.room
+      |> Chat.change_room(room_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
