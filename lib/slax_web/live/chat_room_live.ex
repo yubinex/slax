cc322716fbb98c95ae93ebc4d5e7c6f6f96172ec
yubinex <janhauck99@gmail.com>
2025-09-27 12:11:29 +0200

    adjust auth code

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -47,6 +47,28 @@ defmodule SlaxWeb.ChatRoomLive do
             <% end %>
           </div>
         </div>
+        <ul class="relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end">
+          <li class="text-[0.8125rem] leading-6 text-zinc-900">
+            {@current_user.email}
+          </li>
+          <li>
+            <.link
+              href={~p"/users/settings"}
+              class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+            >
+              Settings
+            </.link>
+          </li>
+          <li>
+            <.link
+              href={~p"/users/log_out"}
+              method="delete"
+              class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+            >
+              Log out
+            </.link>
+          </li>
+        </ul>
       </div>
     </div>
     """
--- lib/slax_web/router.ex
+++ lib/slax_web/router.ex
@@ -17,14 +17,6 @@ defmodule SlaxWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", SlaxWeb do
-    pipe_through :browser
-
-    live "/", ChatRoomLive
-    live "/rooms/:id", ChatRoomLive
-    live "/rooms/:id/edit", ChatRoomLive.Edit
-  end
-
   # Other scopes may use custom stacks.
   # scope "/api", SlaxWeb do
   #   pipe_through :api
@@ -68,6 +60,9 @@ defmodule SlaxWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{SlaxWeb.UserAuth, :ensure_authenticated}] do
+      live "/", ChatRoomLive
+      live "/rooms/:id", ChatRoomLive
+      live "/rooms/:id/edit", ChatRoomLive.Edit
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
     end
