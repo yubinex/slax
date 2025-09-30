e7dfa642ed0c610ffa2309a1d0bd2703734485b6
yubinex <janhauck99@gmail.com>
2025-09-30 23:52:19 +0200

    convert ChatRoomLive to use streams

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -71,8 +71,8 @@ defmodule SlaxWeb.ChatRoomLive do
           </li>
         </ul>
       </div>
-      <div class="flex flex-col grow overflow-auto">
-        <.message :for={message <- @messages} message={message} />
+      <div id="room-messages" class="flex flex-col grow overflow-auto" phx-update="stream">
+        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
       </div>
       <div class="h-12 bg-white px-4 pb-4">
         <.form
@@ -100,11 +100,12 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  attr :dom_id, :string, required: true
   attr :message, Message, required: true
 
   defp message(assigns) do
     ~H"""
-    <div class="relative flex px-4 py-3">
+    <div id={@dom_id} class="relative flex px-4 py-3">
       <div class="h-10 w-10 rounded shrink-0 bg-slate-300"></div>
       <div class="ml-2">
         <div class="-mt-1">
@@ -164,10 +165,10 @@ defmodule SlaxWeb.ChatRoomLive do
      socket
      |> assign(
        hide_topic?: false,
-       messages: messages,
        page_title: "#" <> room.name,
        room: room
      )
+     |> stream(:messages, messages, reset: true)
      |> assign_message_form(Chat.change_message(%Message{}))}
   end
 
@@ -182,7 +183,7 @@ defmodule SlaxWeb.ChatRoomLive do
       case Chat.create_message(room, message_params, current_user) do
         {:ok, message} ->
           socket
-          |> update(:message, &(&1 ++ [message]))
+          |> stream_insert(:messages, message)
           |> assign_message_form(Chat.change_message(%Message{}))
 
         {:error, changeset} ->
